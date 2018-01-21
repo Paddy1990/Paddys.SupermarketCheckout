81ece915fceda7108db7c7ea90696c446ebeb469
Paddy <paddy.halle@outlook.com>
2018-01-21 16:43:49 +0000

    Started fleshing out the services project

---
+++ Paddys.SupermarketCheckout.Services/Data/ISupermarketDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paddys.SupermarketCheckout.Services.Data
+{
+    public interface ISupermarketDatabase
+    {
+    }
+}
---
+++ Paddys.SupermarketCheckout.Services/Data/SupermarketDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paddys.SupermarketCheckout.Services.Data
+{
+    public class SupermarketDatabase : ISupermarketDatabase
+    {
+    }
+}
